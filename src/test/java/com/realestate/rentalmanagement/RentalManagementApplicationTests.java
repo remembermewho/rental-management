ffea1551c7e5775e406e03f4c3726a30c9dc55ad
remembermewho <nurmanbetovasman@gmail.com>
2025-04-05 22:19:12 +0600

    Initial commit

---
+++ src/test/java/com/realestate/rentalmanagement/RentalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.rentalmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
